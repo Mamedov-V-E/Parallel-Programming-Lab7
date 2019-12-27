f53decb5191e313eb34443c167f4dcd13b24545a
Mamedov-V-E <black_snake666@mail.ru>
2019-12-28 02:06:23 +0300

    Scripted auto-commit on change (2019-12-28 02:06:23) by gitwatch.sh

--- src/main/java/Cache.java
+++ src/main/java/Cache.java
@@ -1,7 +1,8 @@
 public class Cache {
 
     public static void main(String[] args) {
-        if (ParseUtils.RUN_CACHE_PATTERN) {
+        if (args.length != 3 ||
+                ParseUtils.getCommandType(args[0]+args[1]+args[2]) != ParseUtils.CommandType.RUN_CACHE) {
             System.out.println("incorrect command-line arguments");
             System.exit(-1);
         }
