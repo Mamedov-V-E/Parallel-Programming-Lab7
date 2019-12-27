42eae57731362cc3e6a5890afea44e3f3fa3e3be
Mamedov-V-E <black_snake666@mail.ru>
2019-12-28 02:35:43 +0300

    Scripted auto-commit on change (2019-12-28 02:35:43) by gitwatch.sh

--- src/main/java/Cache.java
+++ src/main/java/Cache.java
@@ -8,14 +8,15 @@ public class Cache {
     }
 
     public static void main(String[] args) {
-        if (args.length != 2 ||
-                ParseUtils.getCommandType(args[0]+args[1]) != ParseUtils.CommandType.RUN_CACHE) {
+        if (args.length != 3 ||
+                ParseUtils.getCommandType(args[0]+args[1]+args[2]) != ParseUtils.CommandType.RUN_CACHE) {
             System.out.println("incorrect command-line arguments");
             System.exit(-1);
         }
 
-        int minKey = Integer.parseInt(args[0]);
-        int maxKey = Integer.parseInt(args[1]);
+        String port = args[0];
+        int minKey = Integer.parseInt(args[1]);
+        int maxKey = Integer.parseInt(args[2]);
 
         ZMQ.Context context = ZMQ.context(1);
         ZMQ.Socket dealer = context.socket(SocketType.DEALER);
