7f205e8d3af496542b9d54d1bd3959b25fbfda88
Mamedov-V-E <black_snake666@mail.ru>
2019-12-28 01:38:25 +0300

    Scripted auto-commit on change (2019-12-28 01:38:25) by gitwatch.sh

--- src/main/java/CommandsService.java
+++ src/main/java/CommandsService.java
@@ -4,6 +4,10 @@ public class CommandsService {
     public static final Pattern GET_COMMAND_PATTERN = Pattern.compile("^GET \\d+&", Pattern.CASE_INSENSITIVE);
     public static final Pattern PUT_COMMAND_PATTERN = Pattern.compile("^PUT \\d+ \\d+&", Pattern.CASE_INSENSITIVE);
 
-    public enum CommandType
-    {}
+    public enum CommandType {
+        GET,
+        PUT
+    }
+
+    
 }
