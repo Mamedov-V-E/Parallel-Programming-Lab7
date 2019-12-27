81e42fccc8324cad26bf630ac65cc839b6a575b7
Mamedov-V-E <black_snake666@mail.ru>
2019-12-27 15:18:40 +0300

    Scripted auto-commit on change (2019-12-27 15:18:40) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,10 +1,12 @@
+import org.zeromq.ZMQ;
+
 import java.util.Scanner;
 
 public class Client {
     public static final String PROXI_NAME = "proxi";
 
     public static void main(String[] args) {
-        Context context = ZMQ.context(1);
+        ZMQ.Context context = ZMQ.context(1);
 
         Scanner sc = new java.util.Scanner(System.in);
         while (true) {
