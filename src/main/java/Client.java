d3572d82caea67d6ec0b34e12d63c7c97de7b2e3
Mamedov-V-E <black_snake666@mail.ru>
2019-12-27 15:17:36 +0300

    Scripted auto-commit on change (2019-12-27 15:17:36) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,6 +4,7 @@ public class Client {
     public static final String PROXI_NAME = "proxi";
 
     public static void main(String[] args) {
+        
 
         Scanner sc = new java.util.Scanner(System.in);
         while (true) {
