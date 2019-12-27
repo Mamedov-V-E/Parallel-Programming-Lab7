aae9748210e8aad37ad1f7762d5bd02ae057cdfa
Mamedov-V-E <black_snake666@mail.ru>
2019-12-28 02:34:10 +0300

    Scripted auto-commit on change (2019-12-28 02:34:10) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -7,7 +7,7 @@ public class Client {
     public static void main(String[] args) {
         ZMQ.Context context = ZMQ.context(1);
         ZMQ.Socket requester = context.socket(SocketType.REQ);
-        requester.connect(Proxi.ROUTER_ADDRES);
+        requester.connect(Proxi.CLIENT_ROUTER_ADDRES);
         System.out.println("launch and connect client");
         Scanner sc = new java.util.Scanner(System.in);
         while (true) {
--- src/main/java/Proxi.java
+++ src/main/java/Proxi.java
@@ -1,4 +1,4 @@
 public class Proxi {
-    public static final String ROUTER_ADDRES = "tcp//localhost:8080";
+    public static final String CLIENT_ROUTER_ADDRES = "tcp//localhost:8080";
     public static final String DEALER_ADDRES = "tcp//localhost:8080";
 }
