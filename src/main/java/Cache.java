24f88645e99ec86d6c7b4debcf7f6ff83742b093
Mamedov-V-E <black_snake666@mail.ru>
2019-12-28 01:59:38 +0300

    Scripted auto-commit on change (2019-12-28 01:59:38) by gitwatch.sh

--- src/main/java/Cache.java
+++ src/main/java/Cache.java
@@ -3,7 +3,8 @@ public class Cache {
     public static void main(String[] args) {
         if (args.length != 3) {
             System.out.println("incorrect command-line arguments");
-            
+            System.exit(-1);
         }
+        
     }
 }
