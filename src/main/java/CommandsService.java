dde5251494ec406025c91284203d9a878246159c
Mamedov-V-E <black_snake666@mail.ru>
2019-12-28 01:38:01 +0300

    Scripted auto-commit on change (2019-12-28 01:38:01) by gitwatch.sh

--- src/main/java/CommandsService.java
+++ src/main/java/CommandsService.java
@@ -1,5 +1,7 @@
 import java.util.regex.Pattern;
 
 public class CommandsService {
-    public static final Pattern GET_COMMAND_PATTERN = Pattern.compile("^GET \\d+&");
+    public static final Pattern GET_COMMAND_PATTERN = Pattern.compile("^GET \\d+&", Pattern.CASE_INSENSITIVE);
+    public static final Pattern PUT_COMMAND_PATTERN = Pattern.compile("^PUT \\d+ \\d+&", Pattern.CASE_INSENSITIVE);
+    
 }
