472b029887e83ee64fc1a6387e1fe247bef9a025
Mamedov-V-E <black_snake666@mail.ru>
2019-12-28 03:15:06 +0300

    Scripted auto-commit on change (2019-12-28 03:15:06) by gitwatch.sh

--- src/main/java/Proxi.java
+++ src/main/java/Proxi.java
@@ -1,4 +1,5 @@
 public class Proxi {
     public static final String CLIENT_ROUTER_ADDRES = "tcp//localhost:8080";
     public static final String DEALER_ADDRES = "tcp//localhost:8080";
+    public static final Long HEARTBEAT_TIMEOUT = 
 }
