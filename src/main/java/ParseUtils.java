9999cdad333ac0fb900cc8cb4ddb51c036302189
Mamedov-V-E <black_snake666@mail.ru>
2019-12-28 04:27:28 +0300

    Scripted auto-commit on change (2019-12-28 04:27:28) by gitwatch.sh

--- src/main/java/ParseUtils.java
+++ src/main/java/ParseUtils.java
@@ -6,6 +6,8 @@ public class ParseUtils {
     public static final Pattern GET_COMMAND_PATTERN = Pattern.compile("^GET \\d+&", Pattern.CASE_INSENSITIVE);
     public static final Pattern PUT_COMMAND_PATTERN = Pattern.compile("^PUT \\d+ \\d+&", Pattern.CASE_INSENSITIVE);
     public static final Pattern RETURN_VALUE_COMMAND_PATTERN = Pattern.compile("^RETURN_VALUE \\d+&", Pattern.CASE_INSENSITIVE);
+    public static final Pattern CONNECT_COMMAND_PATTERN = Pattern.compile("^CONNECT&", Pattern.CASE_INSENSITIVE);
+    public static final Pattern NOTIFY_COMMAND_PATTERN = Pattern.compile("^NOTIFY+&", Pattern.CASE_INSENSITIVE);
     public static final Pattern RUN_CACHE_PATTERN = Pattern.compile("^\\d+ \\d+ \\d+&");
     public static final String DELIMITER = " ";
 
