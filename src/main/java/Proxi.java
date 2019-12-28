d45acb57320dd03a444e394ad9cec2571537de2c
Mamedov-V-E <black_snake666@mail.ru>
2019-12-28 05:11:56 +0300

    Scripted auto-commit on change (2019-12-28 05:11:56) by gitwatch.sh

--- src/main/java/Proxi.java
+++ src/main/java/Proxi.java
@@ -6,8 +6,8 @@ import org.zeromq.ZMsg;
 import java.util.ArrayList;
 
 public class Proxi {
-    public static final String CLIENT_ROUTER_ADDRES = "tcp//localhost:8080";
-    public static final String CACHE_ROUTER_ADDRES = "tcp//localhost:8081";
+    public static final String CLIENT_ROUTER_ADDRES = "tcp://localhost:8080";
+    public static final String CACHE_ROUTER_ADDRES = "tcp://localhost:8081";
     public static final int HEARTBEAT_TIMEOUT = 5000;
 
     private static final ArrayList<CacheLine> cacheServers = new ArrayList<>();
