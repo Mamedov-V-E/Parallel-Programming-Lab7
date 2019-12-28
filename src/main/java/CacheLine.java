1162c1cd0c366fbabf6d98556d718ffaaca194d7
Mamedov-V-E <black_snake666@mail.ru>
2019-12-28 03:29:09 +0300

    Scripted auto-commit on change (2019-12-28 03:29:09) by gitwatch.sh

--- src/main/java/CacheLine.java
+++ src/main/java/CacheLine.java
@@ -1,3 +1,41 @@
+import org.zeromq.ZFrame;
+
 public class CacheLine {
-    
+    private String id;
+    private ZFrame address;
+    private int minKey;
+    private int maxKey;
+    private long hertbeatTime;
+
+    public CacheLine(String id, ZFrame address, int minKey, int maxKey, long hertbeatTime) {
+        this.id = id;
+        this.address = address;
+        this.minKey = minKey;
+        this.maxKey = maxKey;
+        this.hertbeatTime = hertbeatTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public ZFrame getAddress() {
+        return address;
+    }
+
+    public int getMinKey() {
+        return minKey;
+    }
+
+    public int getMaxKey() {
+        return maxKey;
+    }
+
+    public void setHertbeatTime(long hertbeatTime) {
+        this.hertbeatTime = hertbeatTime;
+    }
+
+    public boolean isDead() {
+        return System.currentTimeMillis() >
+    }
 }
