5e4d98716a13c12accb33896961162e532c1c0fb
Mamedov-V-E <black_snake666@mail.ru>
2019-12-28 04:05:28 +0300

    Scripted auto-commit on change (2019-12-28 04:05:28) by gitwatch.sh

--- src/main/java/Proxi.java
+++ src/main/java/Proxi.java
@@ -60,31 +60,8 @@ public class Proxi {
         items.register(backend, ZMQ.Poller.POLLERR);
 
         while (!Thread.currentThread().isInterrupted()) {
-            items.poll(HEARTBEAT_TIMEOUT);
 
-            if (items.pollin(0)) {
-                ZMsg msg = ZMsg.recvMsg(frontend);
-                String command = new String(msg.getLast().getData(), ZMQ.CHARSET);
-                ParseUtils.CommandType commandType = ParseUtils.getCommandType(command);
-                boolean isIdValid;
-
-                if (commandType == ParseUtils.CommandType.GET) {
-                    Integer id = ParseUtils.getKey(command);
-                    isIdValid = sendGetRequest(backend, id, msg);
-
-                    if (!isIdValid) {
-                        msg.getLast().reset("id is out of cached range");
-                        msg.send(frontend);
-                    }
-                }
-
-                if (commandType == ParseUtils.CommandType.PUT) {
-                    Pair<Integer, Integer> pair = ParseUtils.getKeyValue(command);
-                    isIdValid = sendGetRequest(backend, pair.getKey(), msg);
-                    
-                }
-            }
-
+            
         }
     }
 }
