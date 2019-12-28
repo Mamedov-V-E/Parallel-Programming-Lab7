c04bfe12f15ba14666b081add35d731fdec80121
Mamedov-V-E <black_snake666@mail.ru>
2019-12-28 05:11:36 +0300

    Scripted auto-commit on change (2019-12-28 05:11:36) by gitwatch.sh

--- src/main/java/ParseUtils.java
+++ src/main/java/ParseUtils.java
@@ -1,5 +1,3 @@
-import javafx.util.Pair;
-
 import java.util.regex.Pattern;
 
 public class ParseUtils {
--- src/main/java/Proxi.java
+++ src/main/java/Proxi.java
@@ -1,4 +1,3 @@
-import javafx.util.Pair;
 import org.zeromq.SocketType;
 import org.zeromq.ZFrame;
 import org.zeromq.ZMQ;
