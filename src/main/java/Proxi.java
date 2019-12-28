75c7e001d8ad4c60cb9d230905276f55981fbc04
Mamedov-V-E <black_snake666@mail.ru>
2019-12-28 05:59:09 +0300

    Scripted auto-commit on change (2019-12-28 05:59:09) by gitwatch.sh

--- src/main/java/Proxi.java
+++ src/main/java/Proxi.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class Proxi {
     public static final String CLIENT_ROUTER_ADDRESS = "tcp://localhost:8080";
     public static final String CACHE_ROUTER_ADDRESS = "tcp://localhost:8081";
-    public static final int HEARTBEAT_TIMEOUT = 150;
+    public static final int HEARTBEAT_TIMEOUT = 1500;
 
     private static final ArrayList<CacheLine> cacheServers = new ArrayList<>();
 
