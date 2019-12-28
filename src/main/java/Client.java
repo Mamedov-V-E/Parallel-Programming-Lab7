625afa7843b2b1d0d7605d1612d69bbd791634fb
Mamedov-V-E <black_snake666@mail.ru>
2019-12-28 05:24:23 +0300

    Scripted auto-commit on change (2019-12-28 05:24:23) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -20,7 +20,7 @@ public class Client {
     private static void ExecuteCommand(ZMQ.Socket requester, String commandLine) {
         if (ParseUtils.getCommandType(commandLine) == ParseUtils.CommandType.GET ||
                 ParseUtils.getCommandType(commandLine) == ParseUtils.CommandType.PUT) {
-
+            System.out.println("valid command!");
             requester.send(commandLine);
             String response = requester.recvStr();
 
